35b1c0dff3bc3ce96137a4b64e05df24c1a63d03
anthonymaitz <anthonymaitz@users.noreply.github.com>
2026-04-29 05:12:43 +0000

    deploy: e7483b095402a3a31e110cc1fd58a757d6d017fd

---
+++ jetpack-geography/assets/tgaTextureLoader-D2d12P4d.js
@@ -0,0 +1,1 @@
+import{lq as r,vm as n}from"./index-DumfV9U4.js";class f{constructor(){this.supportCascades=!1}loadCubeData(){throw".env not supported in Cube."}loadData(e,t,o){const s=new Uint8Array(e.buffer,e.byteOffset,e.byteLength),a=r(s);o(a.width,a.height,t.generateMipMaps,!1,()=>{n(t,s)})}}export{f as _TGATextureLoader};
